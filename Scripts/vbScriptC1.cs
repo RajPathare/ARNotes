36f996c9694377b21f614b960c3ed65915c130a4
Raj Pathare <rajgpathare@gmail.com>
2018-06-16 23:23:08 +0530

    Add files via upload

---
+++ Scripts/vbScriptC1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbScriptC1 : MonoBehaviour,IVirtualButtonEventHandler {
+
+    private GameObject vbButtonObject;
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
+
+	// Use this for initialization
+	void Start () {
+
+        vbButtonObject = GameObject.Find("C1Note");
+        vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        MusicSource.clip = MusicClip;
+		
+	}
+
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb) {
+
+        Debug.Log("Button down");
+        MusicSource.Play();
+
+
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb) {
+
+        Debug.Log("Button up");
+
+    }
+	
+}
